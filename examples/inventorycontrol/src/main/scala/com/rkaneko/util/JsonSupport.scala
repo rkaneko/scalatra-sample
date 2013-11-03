98bfbc9a28de1a3f44646338d2015cea8373f0d3
rkaneko <kanek.o.dev@gmail.com>
2013-11-03 17:45:50 +0900

    added bootstrap3.

--- examples/inventorycontrol/src/main/scala/com/rkaneko/util/JsonSupport.scala
+++ examples/inventorycontrol/src/main/scala/com/rkaneko/util/JsonSupport.scala
@@ -1,11 +1,12 @@
 package com.rkaneko.util
 
 import org.json4s.{DefaultFormats, Formats}
+import org.json4s.ext.JodaTimeSerializers
 import org.scalatra.json._
 
 trait JsonSupport extends JacksonJsonSupport {
 
-  protected implicit val jsonFormats: Formats = DefaultFormats
+  protected implicit val jsonFormats: Formats = DefaultFormats ++ JodaTimeSerializers.all
 
   val contentTypeJson = formats("json")
 }
